94e4fbffeb12179d1e256ebfecd9e8def2a42ad8
David Nguyen <phucnguyen@iowa.edu>
2019-04-02 19:05:52 -0500

    part 2 done

--- src/iterators/IntTransform.java
+++ src/iterators/IntTransform.java
@@ -6,21 +6,24 @@ import java.util.function.Function;
 
 public class IntTransform implements Iterator<Integer> {
         // The function that will be applied to each input element to make an output element
-		private final Function<Integer, Integer> f=null; // WHEN STARTING TO CODE REMOVE =null
+		private final Function<Integer, Integer> f; // WHEN STARTING TO CODE REMOVE =null
 
         // The Iterator that this Transform object will get its input from
-		private final Iterator<Integer> input=null;		// WHEN STARTING TO CODE REMOVE =null
+		private final Iterator<Integer> input;	// WHEN STARTING TO CODE REMOVE =null
 
 		public IntTransform(Function<Integer, Integer> f, Iterator<Integer> input) {
+			this.f = f;
+			this.input = input;
+
 		}
 
 		@Override
 		public boolean hasNext() {
-			return false;
+			return input.hasNext();
 		}
 
 		@Override
 		public Integer next() {
-			return null;
+			return f.apply(input.next());
 		}
 }
